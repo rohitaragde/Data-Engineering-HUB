88cb6bfabda0cdf1df7f53eab5cac335a295c61d
Rohit Annasaheb Ragde <32512875+rohitaragde@users.noreply.github.com>
2025-04-04 13:13:34 -0400

    Update Problem No 17- Leetcode_1075.sql

--- SQL50/Problem No 17- Leetcode_1075.sql
+++ SQL50/Problem No 17- Leetcode_1075.sql
@@ -110,6 +110,12 @@ from Employee101 e left join Project101 p
 on e.employee_id=p.employee_id
 group by p.project_id;
 
+select p.project_id,
+round(sum(e.experience_years)/count(e.employee_id),2) as average_years
+from Project p left join Employee e
+on p.employee_id=e.employee_id
+group by p.project_id
+order by 1
 
 
 
