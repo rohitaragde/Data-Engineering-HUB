d60652427df9000d6e70dda7919b92085d8a5071
Rohit Annasaheb Ragde <32512875+rohitaragde@users.noreply.github.com>
2025-04-14 17:16:21 -0400

    Update Problem No 25- Leetcode_1070.sql

--- SQL50/Problem No 25- Leetcode_1070.sql
+++ SQL50/Problem No 25- Leetcode_1070.sql
@@ -111,6 +111,21 @@ where (product_id,yyear) in
 from SalesI group by product_id)
 order by 1;
 
+Better performance:-
+
+SELECT s.product_id,
+       s.year AS first_year,
+       s.quantity,
+       s.price
+FROM Sales s
+JOIN (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) sub ON s.product_id = sub.product_id AND s.year = sub.first_year
+ORDER BY s.product_id;
+
+
 
 
 
